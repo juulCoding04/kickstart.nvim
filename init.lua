4cb7b4404c669e3fc5084f1e220d3957f9d0476c
Juul <juul20018@gmail.com>
2025-07-31 18:28:39 +0200

    adding autopair brackets and getting platformio to work

--- init.lua
+++ init.lua
@@ -248,7 +248,13 @@ rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+  'normen/vim-pio',
 
+  { 
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true
+  },
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
@@ -671,9 +677,9 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
