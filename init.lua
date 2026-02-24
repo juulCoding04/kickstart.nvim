4539ecf45691dc9a2f42facbdaf6c2784cb5a251
Juul <juul20018@gmail.com>
2026-02-24 12:03:18 +0100

    adding side navigation bar and playing around with settings

--- init.lua
+++ init.lua
@@ -85,7 +85,7 @@ vim.o.signcolumn = 'yes'
 -- Decrease update time
 vim.o.updatetime = 250
 
--- Decrease mapped sequence wait time
+-- decrease mapped sequence wait time
 vim.o.timeoutlen = 300
 
 -- Configure how new splits should be opened
@@ -633,6 +633,7 @@ require('lazy').setup({
         pyright = {},
         html = {},
         cssls = {},
+        asm_lsp = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -936,7 +937,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
